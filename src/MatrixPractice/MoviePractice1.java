1a9b722c842e00cba964d6b37d617eb2c7e74190
DanniaG <DanniaG@DanniaG-PC.fios-router.home>
2021-12-23 14:28:28 -0500

    Practice/GitHub

---
+++ src/MatrixPractice/MoviePractice1.java
@@ -0,0 +1,31 @@
+package MatrixPractice;
+
+public class MoviePractice1 {
+	
+	public static void main (String[] args) {
+		
+	    int Movie = 3;
+		
+		switch (Movie) {
+		
+		case 1:
+		
+		 System.out.println("Best Matrix Movie 1");
+		 break;
+		 
+		case 2:
+		
+		 System.out.println("Best Matrix Movie 2");
+		  break;
+		  
+		case 3:
+		
+		 System.out.println(" Best Matrix Movie 3");
+		
+		default:
+			System.out.println(" Best Matrix movie || Deep Message");
+		}
+	}
+	
+	
+}
